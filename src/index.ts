9be25a493ce336a51ffcc8a2c30bbe5c091f3226
hasina <hasinafitiavana2002@gmail.com>
2023-09-14 15:54:41 +0200

    modification texte

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { createConnection } from 'typeorm';
 import express from 'express';
 import cors from 'cors';
 import 'reflect-metadata';
-import userRoutes from './controller/Test';
+import userRoutes from './controller/UtilisateurController';
 import bodyParser from 'body-parser';
 import { Server } from 'socket.io';
 import http from 'http';
@@ -24,48 +24,52 @@ app.use(cors({
         }
     },
 }));
+
+const port = 5000;
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: allowedOrigins,
+        methods: ['GET', 'POST'],
+    },
+});
+
+io.on('connection', (socket) => {
+    console.log('Client connected');
+
+    socket.on('sendMessage', (message) => {
+        // Enregistrez le message dans la base de données si nécessaire
+        // Puis émettez le message à tous les clients connectés
+        io.emit('newMessage', message);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
+// Utilisation du routeur pour les routes utilisateur
+app.use('/utilisateur', userRoutes);
+
+// Utilisation du routeur pour les routes de message
+const messageRouter = messageRoutes(io);
+app.use('/message', messageRouter);
+
+app.get('/', (req, res) => {
+    res.send('Hey this is my API running 🥳');
+});
+
+// Connexion à la base de données
 createConnection()
     .then(() => {
-
-        const server = http.createServer(app);
-
-        // const io = new Server(server, {
-        //     cors: {
-        //         origin: allowedOrigins,
-        //         methods: ['GET', 'POST'],
-        //     },
-        // });
-
-        // io.on('connection', (socket) => {
-        //     console.log('Client connected');
-
-        //     socket.on('sendMessage', (message) => {
-        //         // Enregistrez le message dans la base de données si nécessaire
-        //         // Puis émettez le message à tous les clients connectés
-        //         io.emit('newMessage', message);
-        //     });
-
-        //     socket.on('disconnect', () => {
-        //         console.log('Client disconnected');
-        //     });
-        // });
-
-        // Utilisation du routeur pour les routes utilisateur
-        // app.use('/utilisateur', userRoutes);
-
-        // Utilisation du routeur pour les routes de message
-        // const messageRouter = messageRoutes(io);
-        // app.use('/message', messageRouter);
-        
+        server.listen(port, () => {
+            console.log(`Serveur en cours d'exécution sur le port ${port}`);
+        });
     })
     .catch((error) => {
         console.error('Erreur de connexion à la base de données : ', error);
     });
-    const port = 5000;
-    app.get('/', (req, res) => {
-        res.send('Hey this is my API running 🥳')
-      })
-    app.listen(port, () => {
-        console.log(`Serveur en cours d'exécution sur le port ${port}`);
-    });
-module.exports = app
+
+module.exports = app;
